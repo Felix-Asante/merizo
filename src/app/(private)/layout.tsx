568a95562214e3e846b856cb30071255bafc38ad
felix asante <webdevwithfelix@gmail.com>
2026-03-05 13:01:55 +0000

    redirect to login

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,9 +1,16 @@
 import { DashboardLayout } from "@/ui/shared/layouts/dashboard-layout";
+import { isAuthenticated } from "@/lib/auth/server";
+import { redirect } from "next/navigation";
 
-export default function PrivateLayout({
+export default async function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isAuth = await isAuthenticated();
+  if (!isAuth) {
+    redirect("/login");
+  }
+
   return <DashboardLayout>{children}</DashboardLayout>;
 }
