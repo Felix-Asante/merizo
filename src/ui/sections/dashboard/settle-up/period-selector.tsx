cff1b539c36e8937b6e32e4db0489efed2e78ca3
felix asante <webdevwithfelix@gmail.com>
2026-04-14 12:08:04 +0100

    show all open  settlement

--- src/ui/sections/dashboard/settle-up/period-selector.tsx
+++ src/ui/sections/dashboard/settle-up/period-selector.tsx
@@ -39,12 +39,12 @@ export function PeriodSelector({
       transition={{ duration: 0.2 }}
       className="flex gap-2 overflow-x-auto pb-1 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
     >
-      {/* <PeriodChip
+      <PeriodChip
         label="Open Balance"
         icon={<WalletIcon className="size-3.5" />}
         isActive={selected === "open"}
         onClick={() => onSelect("open")}
-      /> */}
+      />
       {sortedPeriods.map((period) => {
         const status = getPeriodSettlementStatus(debts, period.id);
         return (
